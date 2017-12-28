248df58fa9b0287cefc3795f6507f1def00d0c2d
Ben Jones <ben.james.jones@gmail.com>
2017-12-28 11:14:17 -0700

    MIT License this

--- quatSVD.hpp
+++ quatSVD.hpp
@@ -1,4 +1,25 @@
 #pragma once
+/*
+This code is MIT Licensed.  See below.  If this ends up being useful to you, I'd appreciate it if you let me know.
+
+Copyright 2017 Ben Jones
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+and associated documentation files (the "Software"), to deal in the Software without restriction, 
+including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, 
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 
 /* compute the SVD of a 3x3 matrix and return 
    U, V as quaternions, and Sigma as a Vec3
